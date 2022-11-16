8d66f522cba2474f2f6a04221e78fed7673bbc11
tuannguyen0607 <nguyenanhtuan06071997@gmail.com>
2022-11-16 09:34:17 +0700

    done

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+
 @EnableWebMvc
 @ComponentScan("controller")
 public class WebConfig implements ApplicationContextAware {
